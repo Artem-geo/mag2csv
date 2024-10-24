592b4305608b53bbf8967f85085facabe23d4243
Artem Gorbunov <tl100756@gmail.com>
2024-10-24 17:08:17 +0100

    additional folder restructure

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main() {
     char* argv[2];
     std::string s1 = "mag2csv.exe";
     argv[0] = s1.data();
-    std::string s2 = "C:/Dev/mag_data_transform/cpp/mag2csv/test/data/input.txt";
+    std::string s2 = "C:/Dev/mag_data_transform/cpp/mag2csv/test/input.txt";
     argv[1] = s2.data();
 
     Mag_input_params input_params = Utils::parse_command_input_file(argc, argv);
